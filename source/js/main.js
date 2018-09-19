082b1f0a7e65b366a90f5a41a43759bd3c0daf06
CRYDEsigN <Cryden_da@mail.ru>
2018-09-19 16:25:56 +0300

    refactoring source

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 
-import topadvert from './../vue/components/Topadvert.vue'
-import circleprogress from './../vue/components/circle-progress.vue'
-
 try {
   window.$ = window.jQuery = require('jquery')
 } catch (e) {}
@@ -12,10 +9,9 @@ Vue.use(VueResource)
 
 new Vue({
   el: '#app',
-  components: {topadvert, circleprogress}
+  components: {}
 })
 
 require('./components/main-menu.js')
 require('./components/font_loader.js')
 require('./components/navigation.js')
-require('./components/service-worker.js')
