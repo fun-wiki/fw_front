e053bd68c473485bfe6e65f3f4ec717c988b68a1
CRYDEsigN <Cryden_da@mail.ru>
2018-10-24 13:37:59 +0300

    update cards

--- source/js/components/card_resizer.js
+++ source/js/components/card_resizer.js
@@ -11,6 +11,12 @@ function card_resize() {
   list_width = ($('.card_list').width());
   max_width = 530;
   column_min = Math.floor(list_width/max_width)+1;
+  column_max = Math.floor(list_width/260)
+  if (column_min<column_max) {
+    column_min = column_max
+  }
   width = (list_width/column_min)
+
   $('.card').css('max-width', width);
+  $('.card').css('min-width', width);
 }
