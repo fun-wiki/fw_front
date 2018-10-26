49b6e94f78756625a1967f30a17b54b52a930876
CRYDEsigN <Cryden_da@mail.ru>
2018-10-26 15:30:46 +0300

    add service-worker

---
+++ source/js/components/service-worker.js
@@ -0,0 +1,14 @@
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    }).catch(function(err) {
+      console.log(err)
+    });
+  });
+} else {
+  console.log('service worker is not supported');
+}
